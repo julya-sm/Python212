ddf21221b32c2938b55ab4be34c95e27b26473bf
yulia-sm <yulka-sm@ya.ru>
2023-05-04 14:21:20 +0300

    SQLAlchemy_1

---
+++ models/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_NAME = 'student.db'
+
+engine = create_engine(f'sqlite:///{DATABASE_NAME}')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+def create_db():
+    Base.metadata.create_all(engine)
+
+
+
+
