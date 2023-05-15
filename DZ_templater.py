4181767d0193f6f4608af48f29e0a398fc01e4c3
yulia-sm <yulka-sm@ya.ru>
2023-05-15 19:10:33 +0300

    templater2

--- DZ_templater.py
+++ DZ_templater.py
@@ -1,27 +1,27 @@
-from jinja2 import Template
-
-pages = [
-    {'tab': "/index", 'text': 'Главная'},
-    {'tab': "/news", 'text': 'Новости'},
-    {'tab': "/about", 'text': 'О компании'},
-    {'tab': "/shop", 'text': 'Магазин'},
-    {'tab': "/contacts", 'text': 'Контакты'},
-]
-
-
-link = """
-<ul>
-{% for p in pages -%}
-{% if p.text == "Главная"-%}
-    <li><a href="{{ p['tab'] }}" class="active">{{ p['text'] }}</a></li>
-{% else -%}
-    <li><a href="{{ p['tab'] }}">{{ p['text'] }}</a></li>
-{% endif -%}
-{% endfor -%}
-</ul>
-"""
-
-tm = Template(link)
-msg = tm.render(pages=pages)
-
-print(msg)
+# from jinja2 import Template
+#
+# pages = [
+#     {'tab': "/index", 'text': 'Главная'},
+#     {'tab': "/news", 'text': 'Новости'},
+#     {'tab': "/about", 'text': 'О компании'},
+#     {'tab': "/shop", 'text': 'Магазин'},
+#     {'tab': "/contacts", 'text': 'Контакты'},
+# ]
+#
+#
+# link = """
+# <ul>
+# {% for p in pages -%}
+# {% if p.text == "Главная"-%}
+#     <li><a href="{{ p['tab'] }}" class="active">{{ p['text'] }}</a></li>
+# {% else -%}
+#     <li><a href="{{ p['tab'] }}">{{ p['text'] }}</a></li>
+# {% endif -%}
+# {% endfor -%}
+# </ul>
+# """
+#
+# tm = Template(link)
+# msg = tm.render(pages=pages)
+#
+# print(msg)
