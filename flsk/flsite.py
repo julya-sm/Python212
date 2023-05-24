bba178313819e733beb371a940443a46b4315174
yulia-sm <yulka-sm@ya.ru>
2023-05-24 18:41:01 +0300

    87-88pairs

--- flsk/flsite.py
+++ flsk/flsite.py
@@ -1,6 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, session, redirect, g, abort
+import os
+import sqlite3
+from FDataBase import FDataBase
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = '8bcfc6b33e295c377ddee4ec8556d43172e44861'
 
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'fjkklkl5k345fjfj0h9he4r3c8'  # код д.б. не менее 16 символов
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsk.db')))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
 
 menu = [
     {'name': 'Главная', 'url': 'index'},
@@ -9,10 +35,44 @@ menu = [
 ]
 
 
-@app.route("/index")  # можно несколько декораторов указывать
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+# @app.route("/index")  # можно несколько декораторов указывать
 @app.route("/")
 def index():
-    return render_template('index.html', title='Главная', menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', title='Главная', menu=dbase.get_menu(), posts=dbase.get_posts_anonce())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', title='Добавление статьи', menu=dbase.get_menu())
+
+
+@app.route("/post/<alias>")
+def show_post(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(alias)
+    if not title:
+        abort(404)
+    return render_template('post.html', title=title, post=post, menu=dbase.get_menu())
 
 
 @app.route("/about")
@@ -25,9 +85,44 @@ def profile(username):
     return f'Пользователь: {username}'
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено успешно', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+        print(request.form)
+        context = {
+            'username': request.form['username'],
+            'email': request.form['email'],
+            'message': request.form['message']
+        }
+        return render_template('contact.html', title='Обратная связь', menu=menu, **context)
     return render_template('contact.html', title='Обратная связь', menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('page404.html', title='Страница не найдена', menu=dbase.get_menu())
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'julia' and request.form['psw'] == '123456':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title='Авторизация ', menu=menu)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 if __name__ == '__main__':
