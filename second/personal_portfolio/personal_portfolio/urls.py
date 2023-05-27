48d407850c105de55eaac0e0a2f76ed3a4e59d6e
yulia-sm <yulka-sm@ya.ru>
2023-05-27 09:51:38 +0300

    Django_second

--- second/personal_portfolio/personal_portfolio/urls.py
+++ second/personal_portfolio/personal_portfolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for password_generator_project project.
+URL configuration for personal_portfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('password/', views.password, name='password'),
 ]
